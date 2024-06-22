c38f7b69bd413a510a911039f92b8e4fbd404ee9
raustin9 <raustin9@vols.utk.edu>
2024-06-22 01:20:57 -0400

    use go.sh script for commands

---
+++ go.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/bash
+
+go-build() {
+    echo "Building everything..."
+    make -f "Makefile.linux.mak" all
+    errorlevel=$?
+    if [ $errorlevel -ne 0 ]
+    then
+        echo "Error: $errorlevel" && exit
+    fi
+
+    echo "All assemblies built"
+}
+
+go-rebuild() {
+    echo "Rebuilding everything..."
+    make -f "Makefile.linux.mak" clean
+    make -f "Makefile.linux.mak" all
+    errorlevel=$?
+    if [ $errorlevel -ne 0 ]
+    then
+        echo "Error: $errorlevel" && exit
+    fi
+
+    echo "All assemblies built"
+}
+
+
+go-clean() {
+    make -f "Makefile.linux.mak" clean
+    
+    echo "Done"
+}
+
+go-valgrind() {
+    valgrind\
+        --leak-check=full\
+        --show-leak-kinds=all\
+        --track-origins=yes\
+        --verbose\
+        --log-file=valgrind-out.txt\
+        ./bin/compiler\
+    ##
+}
+
+# Run the command(s)
+"go-$@"
